c7d8252081e183ead5ddf222a4a34b58df5b9b86
Austin Jess <ajess33@gmail.com>
2019-07-24 22:30:44 -0500

    permissions still isnt working!!!!

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-const styles = StyleSheet.create({
+// "plist": {
+//   "NSLocationWhenInUsageDescription": "Testing message changes"
+// }
+
+styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  }
 });
+
+class App extends React.Component {
+  state = {
+    location: null
+  };
+
+  componentDidMount() {
+    this.checkPermission();
+  }
+
+  checkPermission = async () => {
+    // const ios = { scope: 'whenInUse' };
+    const {
+      status
+      // permissions: {
+      //   location: { ios }
+      // }
+    } = await Permissions.getAsync(Permissions.LOCATION);
+    if (status === 'undetermined') {
+      alert(`Hey look I'm a function that isn't doing my job!`);
+      Linking.openURL('app-settings:');
+      return;
+    } else {
+      return Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>The Snitcher</Text>
+        {/* <Button
+          title="Request Permission"
+          type="standard"
+          onPress={this.checkPermission}
+        /> */}
+      </View>
+    );
+  }
+}
+
+export default App;
